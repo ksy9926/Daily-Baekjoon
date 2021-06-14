ba98740b9767f9ee54aec8a3fc51b70138616219
수영 김 <ksy9926@naver.com>
2021-06-14 21:39:24 +0900

    단어 공부

--- 단어 공부/solution.py
+++ 단어 공부/solution.py
@@ -1,8 +1,40 @@
-from collections import Counter
+word = input()
+word_list = []
+word_dict = {}
 
-a = input()
-cnt_dict = Counter(a)
-stack = []
-print(cnt_dict)
-for key in cnt_dict:
-    print(key)
+for s in word:
+    s = s.upper()
+    if s in word_dict:
+        word_dict[s] += 1
+    else:
+        word_dict[s] = 1
+
+for key, value in word_dict.items():
+    word_list.append([key, value])
+
+word_list.sort(key=lambda x: x[1], reverse=True)
+
+if len(word_list) == 1:
+    print(word_list[0][0])
+elif word_list[0][1] == word_list[1][1]:
+    print('?')
+else:
+    print(word_list[0][0])
+
+
+
+
+# 다른 사람의 풀이
+## [출처] 백준 1157번 단어 공부 파이썬|작성자 dingche
+n = str(input().upper())
+unique = list(set(n))
+
+li = list()
+for i in unique:
+    li.append(n.count(i))
+
+if li.count(max(li)) > 1:
+    print('?')
+else:
+    temp = li.index(max(li))
+    print(unique[temp])
