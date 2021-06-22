c46b2a11902d45ca035c2d096c65c3c49dc29915
수영 김 <ksy9926@naver.com>
2021-06-22 19:32:28 +0900

    1로 만들기

---
+++ DP/1로 만들기/solution.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+d = {1:0, 2:1, 3:1}
+
+for i in range(4, N+1):
+    d[i] = d[i-1] + 1
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3] + 1)
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2] +1)
+
+print(d[N])
